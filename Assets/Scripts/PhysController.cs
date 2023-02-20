c48d8dc8ef45b999d8bb5acca6e6e44af1cf78ad
Steve <steven4.lewis@live.uwe.ac.uk>
2023-02-20 22:08:33 +0000

    raycast setup

---
+++ Assets/Scripts/PhysController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public GameObject capsule;
+
+    private Ray ray;
+    private bool _rayDidHit;
+
+    public float rideHeight;
+    private float gravity;
+    private float spring;
+
+    void CheckForColliders()
+    {
+        ray = new Ray(capsule.transform.position, capsule.transform.up * -1.0f);
+        Debug.DrawRay(capsule.transform.position, Vector3.down * rideHeight, Color.red);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            _rayDidHit = true;
+            Debug.Log(hit.collider.gameObject.name + " was hit");
+        }
+    }
+
+    void Update()
+    {
+        CheckForColliders();
+    }
+}
