fa8346abafcd50e7fe628f6d1907bda711be9b2a
Steve <steven4.lewis@live.uwe.ac.uk>
2023-02-22 23:24:22 +0000

    Camera Stuff finished

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class CameraManager : MonoBehaviour
 {
@@ -9,11 +10,19 @@ public class CameraManager : MonoBehaviour
 
     public Transform targetTransform;       //Object camera follows
     public Transform cameraPivot;             //Object camera pivots on
+    public Transform cameraTransform;    //Transform of actual camera object
+    public LayerMask collisionLayers;        //Layers we want camera to collide with
+    private float defaultPosition;
     private Vector3 cameraFollowVelocity = Vector3.zero;
+    private Vector3 cameraVectorPosition;
 
+    public float cameraCollisionOffset = 0.2f;       //how much camera will jump off objects.
+    public float minimumCollisionOffset = 0.2f;
+    public float cameraCollisionRadius = 0.2f;
     public float cameraFollowSpeed = 0.2f;
     public float cameraLookSpeed = 2f;
     public float cameraPivotSpeed = 2f;
+
 
     public float lookAngle;     //up and down
     public float pivotAngle;    //left and right
@@ -24,12 +33,15 @@ public class CameraManager : MonoBehaviour
     {
         inputManager = FindObjectOfType<InputManager>();
         targetTransform = FindObjectOfType<PlayerManager>().transform;
+        cameraTransform = Camera.main.transform;
+        defaultPosition = cameraTransform.localPosition.z;
     }
 
     public void HandleAllCameraMovement()
     {
         FollowTarget();
         RotateCamera();
+        HandleCameraCollisions();
     }
 
     private void FollowTarget()
@@ -42,13 +54,16 @@ public class CameraManager : MonoBehaviour
 
     private void RotateCamera()
     {
+        Vector3 rotation;
+        Quaternion targetRotation;
+
         lookAngle = lookAngle + (inputManager.cameraInputX * cameraLookSpeed);
         pivotAngle = pivotAngle - (inputManager.cameraInputY * cameraPivotSpeed);
         pivotAngle = Mathf.Clamp(pivotAngle, minPivotAngle, maxPivotAngle);
 
-        Vector3 rotation = Vector3.zero;
+        rotation = Vector3.zero;
         rotation.y = lookAngle;
-        Quaternion targetRotation = Quaternion.Euler(rotation);
+        targetRotation = Quaternion.Euler(rotation);
         transform.rotation = targetRotation;
 
         rotation = Vector3.zero;
@@ -56,4 +71,28 @@ public class CameraManager : MonoBehaviour
         targetRotation= Quaternion.Euler(rotation);
         cameraPivot.localRotation = targetRotation;
     }
+
+    private void HandleCameraCollisions()
+    {
+        float targetPosition = defaultPosition;
+        RaycastHit hit;
+        Vector3 direction = cameraTransform.position - cameraPivot.position;
+        direction.Normalize();
+
+        if (Physics.SphereCast
+            (cameraPivot.transform.position, cameraCollisionRadius, direction, out hit, Mathf.Abs(targetPosition), collisionLayers))
+        {
+            float distance = Vector3.Distance(cameraPivot.position, hit.point);
+            targetPosition = -(distance - cameraCollisionOffset);
+        }
+
+        if (Mathf.Abs(targetPosition) < minimumCollisionOffset)
+        {
+            targetPosition = targetPosition - minimumCollisionOffset;
+        }
+
+        cameraVectorPosition.z = Mathf.Lerp(cameraTransform.localPosition.z, targetPosition, 0.2f);
+        cameraTransform.localPosition = cameraVectorPosition;
+    }
+
 }
