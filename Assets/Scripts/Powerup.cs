1907aa9ae89a41d8ef446651624d109df6b528f0
Steve <steven4.lewis@live.uwe.ac.uk>
2023-04-19 14:58:14 +0100

    more powerup quicksave

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Powerup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float multiplier = 1.4f;
+    public float waitTime = 2f;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Pickup(other));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Pickup(Collider player)
     {
-        
+        //PlayerMotion  playerMotion = player.GetComponent<PlayerMotion>();
+        PhysicsManager physicsManager = player.GetComponent<PhysicsManager>();
+
+        //powerup goes here
+        //playerMotion.runSpeed *= multiplier;
+        physicsManager.jump
+
+
+        gameObject.transform.position = new Vector3(0, -50, 0);
+
+        yield return new WaitForSeconds(waitTime);
+
+        //powerup ends here
+        //playerMotion.runSpeed /= multiplier;
+
+        Destroy(gameObject);
     }
+
 }
